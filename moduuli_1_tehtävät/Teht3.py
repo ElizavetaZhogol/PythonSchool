21834745295b2862adbcfb044cf8cf2c44d5195f
ElizavetaZhogol <liza.zhogol@gmail.com>
2025-08-19 13:21:35 +0300

    #

--- moduuli_1_tehtävät/Teht3.py
+++ moduuli_1_tehtävät/Teht3.py
@@ -1,1 +1,8 @@
-print ("Hello hello")
+# Kirjoita ohjelma, joka kysyy ympyrän säteen ja tulostaa sen pinta-alan.
+
+pi = 3.14
+sade = int(input('Anna sade: '))
+
+pintaAla = pi*sade**2
+print(pintaAla)
+print("Ympyrän pinta-ala on", pintaAla)
