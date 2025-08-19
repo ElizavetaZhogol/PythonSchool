6099ce12532097dfff858933f6a3271ef39904ac
ElizavetaZhogol <liza.zhogol@gmail.com>
2025-08-19 21:26:23 +0300

    #

---
+++ moduuli_1_tehtävät/Teht10.py
@@ -0,0 +1,34 @@
+# Kirjoita ohjelma, joka kysyy käyttäjän biologisen sukupuolen ja hemoglobiiniarvon (g/l).
+# Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen, normaali vai korkea.
+
+# Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
+# Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
+
+sukupuoli = input('\nOletko mies vai nainen? Kirjoita vastauksesi tähän: ')
+hemoglobiiniarvo = int(input("Mikä on hemoglobiiniarvosi? Kirjoita tähän: "))
+
+print("\nOlet " + sukupuoli + ".\n"
+      "Hemoglobiiniarvosi on", hemoglobiiniarvo, ".\n")
+
+if sukupuoli == "nainen":
+    if hemoglobiiniarvo < 117:
+        print("Hemoglobiiniarvosi on alhainen.\n"
+              "Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.\n")
+    elif 117<=hemoglobiiniarvo<=175:
+        print("Hemoglobiiniarvosi on normaali.\n"
+              "Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.\n")
+    elif hemoglobiiniarvo > 175:
+        print("Hemoglobiiniarvosi on korkea.\n"
+              "Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.\n")
+elif sukupuoli == "mies":
+    if hemoglobiiniarvo < 134:
+        print("Hemoglobiiniarvosi on alhainen.\n"
+              "Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.\n")
+    elif 134<=hemoglobiiniarvo<=195:
+        print("Hemoglobiiniarvosi on normaali.\n"
+              "Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.\n")
+    elif hemoglobiiniarvo > 195:
+        print("Hemoglobiiniarvosi on korkea.\n"
+              "Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.\n")
+else:
+    print("Syötit tiedot väärin.\n")
