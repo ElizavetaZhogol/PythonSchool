692cece67c9e365a5d13c1a87a6ad9883b02f7d9
ElizavetaZhogol <liza.zhogol@gmail.com>
2025-08-27 18:31:43 +0300

    #

--- moduuli_4_tehtävät/Teht4_5.py
+++ moduuli_4_tehtävät/Teht4_5.py
@@ -4,31 +4,29 @@
 # Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty.
 # (Oikea käyttäjätunnus on python ja salasana rules).
 
-import getpass
-
-kayttajatunnus = getpass.getuser()
-print(kayttajatunnus)
-
-salasana = getpass.getpass('Anna salasana: ')
-print(salasana)
+kayttajatunnus = "python"
+salasana = "rules"
 
 print("Kirjaudu tilillesi antamalla käyttäjätunnuksesi ja salasanasi")
 
-tilitunnus = input('Anna käyttäjätunnus')
-tilisalasana = input('Anna salasana')
+tilitunnus = input('Anna käyttäjätunnus: ')
+tilisalasana = input('Anna salasana: ')
 
 if kayttajatunnus == tilitunnus and salasana == tilisalasana:
-    print("Olet kirjautunut tilillesi onnistuneesti.")
+    print("Tervetuloa!")
 elif kayttajatunnus != tilitunnus or salasana != tilisalasana:
     tries = 1
     while tries < 5:
         attempts = 5 - tries
         print("Yritä uudelleen. Sinulla on", attempts, "yritystä jäljellä.")
-        tilitunnus = input('Anna käyttäjätunnus')
-        tilisalasana = input('Anna salasana')
+        tilitunnus = input('Anna käyttäjätunnus: ')
+        tilisalasana = input('Anna salasana: ')
         if kayttajatunnus == tilitunnus and salasana == tilisalasana:
-            print("Olet kirjautunut tilillesi onnistuneesti.")
+            print("Tervetuloa!")
             break
         else:
             tries = tries + 1
+    if tries == 5:
+        print("Pääsy evätty")
+    
 
