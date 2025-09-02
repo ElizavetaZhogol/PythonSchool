bc2f134d29b26cf7d48daedcf71dcb73978ad1a1
ElizavetaZhogol <liza.zhogol@gmail.com>
2025-09-02 21:52:50 +0300

    #

--- moduuli_6_tehtävät/Teht6_2.py
+++ moduuli_6_tehtävät/Teht6_2.py
@@ -2,3 +2,26 @@
 # Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa.
 # Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku,
 # joka kysytään käyttäjältä ohjelman suorituksen alussa.
+
+import random
+
+tahko = int(input('\nIlmoita noppasi tahkojen yhteismäärä: '))
+
+print("\nKuinka monta nopanheittoa tarvitaan, jotta saadaan luku",tahko,"?\n")
+
+def noppa(tahkot):
+    arvo = random.randint(1, tahkot)
+    return arvo
+
+luku = noppa(tahko)
+
+n = 1
+
+while luku != tahko:
+    print("Numero", luku, "tuli esiin")
+    n = n + 1
+    luku = noppa(tahko)
+else: 
+   print("Noppa antoi numero", tahko)
+
+print("Se vaati", n ,"yritystä") 
