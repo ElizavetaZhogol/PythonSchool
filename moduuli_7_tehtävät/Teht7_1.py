130f22cce1e2015519dbcaf478f431aab20d922c
ElizavetaZhogol <liza.zhogol@gmail.com>
2025-09-08 19:12:25 +0300

    #

---
+++ moduuli_7_tehtävät/Teht7_1.py
@@ -0,0 +1,23 @@
+# Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron,9
+# jonka jälkeen ohjelma tulostaa sitä vastaavan vuodenajan (kevät, kesä, syksy, talvi).
+# Tallenna ohjelmassasi kuukausia vastaavat vuodenajat merkkijonoina monikkotietorakenteeseen.
+# Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten, että joulukuu on ensimmäinen talvikuukausi.
+
+vuodenajat = "Talvi", "Kevät", "Kesä", "Syksy"
+
+kuukausiNumero = int(input('\nAnna kuukauden numero (1-12): '))
+
+if kuukausiNumero == 12 or kuukausiNumero <= 2:
+    vuodenaika = vuodenajat[0]
+    print("\nKuukausi", kuukausiNumero ,"on " + vuodenaika + " kuukausi")
+elif 2 < kuukausiNumero <= 5:
+    vuodenaika = vuodenajat[1]
+    print("\nKuukausi", kuukausiNumero ,"on " + vuodenaika + " kuukausi")
+elif 5 < kuukausiNumero <= 8:
+    vuodenaika = vuodenajat[2]
+    print("\nKuukausi", kuukausiNumero ,"on " + vuodenaika + " kuukausi")
+elif 8 < kuukausiNumero <= 11:
+    vuodenaika = vuodenajat[3]
+    print("\nKuukausi", kuukausiNumero ,"on " + vuodenaika + " kuukausi")
+else:
+    print("Tällä numerolla ei ole kuukautta")
