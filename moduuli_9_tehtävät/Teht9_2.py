8b2662c6d9fec80512d2b320faee428dcc72e6cc
ElizavetaZhogol <liza.zhogol@gmail.com>
2025-10-22 01:16:48 +0300

    #

--- moduuli_9_tehtävät/Teht9_2.py
+++ moduuli_9_tehtävät/Teht9_2.py
@@ -16,7 +16,7 @@ class Auto:
 
         muutos = self.nopeus + nopeudenMuutos
 
-        if 0 < muutos <= self.huippunopeus:
+        if 0 <= muutos <= self.huippunopeus:
 
            self.nopeus = muutos 
         
